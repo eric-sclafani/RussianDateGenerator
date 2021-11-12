34a3720fa279003dd07a4fc58c139e5a15f2c2d3
gilliangregory <gillian.gregory@stonybrook.edu>
2021-11-12 12:43:52 -0500

    edited regexes

--- regexes.py
+++ regexes.py
@@ -2,11 +2,11 @@
 import re
 month=input("Input a Russian month: ")
 def decline_month(month):
-    if re.search(r"ь$", month) is not None:
+    if re.search(r"ь$", month):
         month=re.sub(r"ь$", "я", month)
-    elif re.search(r"й$", month) is not None:
+    elif re.search(r"й$", month):
         month=re.sub(r"й$", "я", month)
-    elif re.search(r"т$", month) is not None:
+    elif re.search(r"т$", month):
         month=re.sub(r"т$", "та", month)
     return month
 print(decline_month(month)) 
