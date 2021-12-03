87e84688801794eca3a76db4a5f4a8eb4895e7e0
gilliangregory <gillian.gregory@stonybrook.edu>
2021-12-03 12:45:41 -0500

    fixed gregorian_cal

--- regexes.py
+++ regexes.py
@@ -327,7 +327,7 @@ def gregorian_cal(datelist):
                 yearg=year
                 dayg=gregd
         else:
-            gregd=int(gregd)+13
+            gregd=int(day)+13
             if int(gregd)>31:
                 monthg="01"
                 yearg=int(year)+1
